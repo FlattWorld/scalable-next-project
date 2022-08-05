7ee0136cbfa2300ac7a37ad5acbc4adb2242bdab
Lino09 <onosmx@gmail.com>
2022-08-05 15:07:48 -0500

    build: remove tests from pages

--- pages/terms-and-conditions/__tests__/terms-and-conditions.test.jsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react'
-import TermsAndConditions from '../TermsAndConditions'
-
-describe('TermsAndConditions', () => {
-  it('renders without crashing', () => {
-    const { baseElement } = render(<TermsAndConditions />)
-    expect(baseElement).toBeTruthy()
-  })
-})
